a6acae1df12871c8e40b4859afee75b64c127d09
brunamartinslop <27678653-brunamartinslop@users.noreply.replit.com>
2023-10-20 15:26:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("hello world")
+animal_secreto = "Mamute"
+
+dicas = ['grande', 'come mato', 'tem tromba', 'aparece no filme do Era do gelo']
+tentativa = 0
+palpite = ""
+while palpite != animal_secreto:
+    print(f"Dica nº{tentativa}: o animal {dicas[tentativa]}")
+    palpite = input("Adivinhe o animal que estou pensando:")
+    tentativa += 1
+    if tentativa == 4:
+        print("game over")
+        break
+    elif palpite == animal_secreto:
+        print("você acertou!!")
